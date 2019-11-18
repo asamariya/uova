9674c8ba06b7dcd34ec7c5e646188e314b194547
Abhishek Samariya <asamariya@gmail.com>
2019-11-19 10:14:37 +1100

    Show images from contentful and order the menu items

--- contentful.js
+++ contentful.js
@@ -2,15 +2,26 @@ const spaceId = "sxbrtelqz7wh";
 const environmentId = "master";
 const accessToken = "LSqR_bf52ZiU3oBx77kZBBV3zpQ5BEzTK2KWLjkM6ew";
 
-const url = `https://cdn.contentful.com/spaces/${spaceId}/environments/${environmentId}/entries?access_token=${accessToken}`;
+const url = `https://cdn.contentful.com/spaces/${spaceId}/environments/${environmentId}/entries?access_token=${accessToken}&order=-fields.order&content_type=menuItem`;
 
 const sectionTag = document.querySelector("section.grid");
 
-let items;
 const fetchItems = async () => {
   let response = await fetch(url);
   let data = await response.json();
-  items = data.items.map(item => {
+
+  const assets = data.includes.Asset;
+
+  let items = data.items.map(item => {
+    let imageUrl = "./assets/image1.jpg";
+
+    const imageId = item.fields.image.sys.id;
+    const imageData = assets.find(asset => asset.sys.id == imageId);
+
+    if (imageData) {
+      imageUrl = imageData.fields.file.url;
+    }
+    item.fields.image = imageUrl;
     return item.fields;
   });
   return items;
@@ -22,10 +33,15 @@ fetchItems().then(data => {
     sectionTag.innerHTML =
       sectionTag.innerHTML +
       `
-      <div class="item">
-        ${item.title}
-      </div>
-    
-    `;
+        <div class="item">
+          <img src=${item.image}>
+          <div class="title">
+            <h2>${item.title}</h2>
+            <p>${item.price}</p>
+          </div>
+
+          <p>${item.description}</p>
+        </div>
+      `;
   });
 });
